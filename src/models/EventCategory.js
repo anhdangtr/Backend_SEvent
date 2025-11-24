0d83df77571c775b392c143a266c61bb0d890e36
anhdangtr <danganhanh2005@gmail.com>
2025-11-24 16:15:01 +0700

    rm main

--- src/models/EventCategory.js
+++
@@ -1,9 +0,0 @@
-// models/EventCategory.js
-const mongoose = require('mongoose');
-
-const eventCategorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('EventCategory', eventCategorySchema);
